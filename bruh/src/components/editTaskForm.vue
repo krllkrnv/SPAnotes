74ef6d6001d1c82531e3bb1b02ab1c7904eb0e32
krllkrnv <krllkrnv@yandex.ru>
2023-09-20 13:55:19 +0300

    v6

--- bruh/src/components/editTaskForm.vue
+++ bruh/src/components/editTaskForm.vue
@@ -5,6 +5,7 @@
   <v-form @submit.prevent="submitChange" class="scrollable-form">
         Задание:
       <v-text-field
+        @input="pushToHistoryWithDelay"
         v-model="form_task.name"
         :rules="rules"
       ></v-text-field>
@@ -17,12 +18,19 @@
         <th>Название</th>
       </tr>
       <tr v-for="(item, index) in form_task.subtasks">
-        <td><v-checkbox v-model="item.status" label="Выполнено"></v-checkbox></td>
-        <td><v-text-field v-model="item.name"></v-text-field></td>
-        <v-btn :style="{ visibility: showDelBtn ? 'visible' : 'hidden' }" color="error" icon="$error" @click="deleteSubtask(index)" class="delete-button"></v-btn>
+        <td><v-checkbox
+          @change="pushToHistory"
+          v-model="item.status" label="Выполнено"></v-checkbox></td>
+        <td><v-text-field v-model="item.name"
+        @input="pushToHistoryWithDelay"
+        ></v-text-field></td>
+        <v-btn
+          :style="{ visibility: showDelBtn ? 'visible' : 'hidden' }" color="error" icon="$error" @click="deleteSubtask(index)" class="delete-button"></v-btn>
       </tr>
     </table>
-      <v-btn block class="mt-2" type="submit">Изменить задачу</v-btn>
+    <v-btn icon="mdi-undo" class="ma-2 pa-2" @click="undo"></v-btn>
+    <v-btn icon="mdi-redo" class="ma-2 pa-2" @click="redo"></v-btn>
+    <v-btn block class="mt-2" type="submit">Изменить задачу</v-btn>
     </v-form>
   </div>
 </template>
@@ -46,34 +54,71 @@ export default {
         },
       ],
       history: [],
-      showDelBtn: false// история изменений. Просто при каждом пуке пушим объект.
+      showDelBtn: false,
+      inputTimer: 0,
+      historyIndex: 0
+      // история изменений. Просто при каждом пуке пушим объект.
     }
+  },
+  created() {
+    this.pushToHistory()
+    this.historyIndex = this.historyIndex - 1// сразу при инициализации компонента пушим в историю первоначальное состояние.
   },
   props: {
     task: Object,
     required: true
   },
   methods: {
-    pushToStorage() {
-
-    },
     pushToHistory(){
-      this.history.push(this.form_task)
+      this.history.splice(this.historyIndex + 1); // для того чтобы при внесении изменения после отката удалялась лишняя история
+      this.history.push({
+        name: this.form_task.name,
+        subtasks: JSON.parse(JSON.stringify(this.form_task.subtasks))  // поскольку объект передается по ссылке, надо его скопировать, а не передать.
+      })
+      this.historyIndex = this.historyIndex + 1
+      console.table(this.history)
+      console.log(this.historyIndex)
     },
     addSubtask() {
       this.form_task.subtasks.push({
         name: '',
         status: false
-      })
+      });
+      this.pushToHistory()
     },
     deleteSubtask(index) {
-      this.form_task.subtasks.splice(index, 1)
+      this.form_task.subtasks.splice(index, 1);
+      this.pushToHistory()
     },
     submitChange(){
       this.$store.commit('EDIT_TASK', {old_elem_name: this.task.name, new_elem: this.form_task})
     },
     changeShowDelBtn(){
       this.showDelBtn = !this.showDelBtn
+    },
+    pushToHistoryWithDelay(){
+      if (this.inputTimer) {
+        clearTimeout(this.inputTimer);
+      }
+      this.inputTimer = setTimeout(() => {
+        this.pushToHistory();
+      }, 750)
+    },
+    undo(){
+      if (this.historyIndex > 0) {
+        this.historyIndex = this.historyIndex - 1
+        this.form_task = JSON.parse(JSON.stringify(this.history[this.historyIndex]));
+      }
+      console.log(this.historyIndex)
+
+    },
+    redo(){
+      if (this.historyIndex < this.history.length - 1) {
+        this.historyIndex = this.historyIndex + 1
+        this.form_task = JSON.parse(JSON.stringify(this.history[this.historyIndex]));
+      }
+      console.log(this.historyIndex)
+
     }
   }
 }
